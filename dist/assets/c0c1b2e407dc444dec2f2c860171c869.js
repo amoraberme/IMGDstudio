c46d0e846a285078c7d29391f800f221dddbf0a5
unknown <jericoberme29@gmail.com>
2026-05-07 19:59:33 +0800

    Configure Vercel deployment and include static archive files

---
+++ dist/assets/c0c1b2e407dc444dec2f2c860171c869.js
@@ -0,0 +1,3 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,993225,o=>{o.n(o.i(680872))}]);
+
+//# sourceMappingURL=0uvtorhwd035l.js.map
